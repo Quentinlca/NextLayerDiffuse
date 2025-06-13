5d63a70c924bfc0622bc95454ddfc0c25febd64b
Quentin Leca <quentin.leca@polytechnique.edu>
2025-06-13 13:44:48 +0200

    Main Dataset generator script ready for generation

--- dataset_generator/DatasetGeneration.py
+++ dataset_generator/DatasetGeneration.py
@@ -2,9 +2,9 @@ from src.DatasetGenerators import *
 import os
 
 if __name__ == '__main__':
-    generator = NextTokenGenerator.NextTokenGenerator('modular_character',
+    generator = NextTokenGenerator('modular_characters',
                                                       output_image_size=128,
                                                       save_size=1000)
     
-    generator.generate(os.path.abspath('characters.json'),
+    generator.generate('characters.json',
                        resume=True)
