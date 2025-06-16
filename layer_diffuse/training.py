950f10bf413bd5a9a5d0b38cafdfbbff8f473fc5
Quentinlca <quentin.leca@polytechnique.edu>
2025-06-16 19:03:35 +0200

    reformated the main file without training loop function

--- layer_diffuse/training.py
+++ layer_diffuse/training.py
@@ -1,15 +1,11 @@
 from models import DDPMNextTokenV1
 from data_loaders import ModularCharatersDataLoader
-from PIL import Image
-dataset_name = "QLeca/modular_charactersv2"
-val_split = f"train"
-train_split = f"train"
-
-
-from safetensors import safe_open
 import argparse
 
-if __name__ == "__main__":
+def train_loop():
+    dataset_name = "QLeca/modular_charactersv2"
+    val_split = f"train"
+    train_split = f"train"
     parser = argparse.ArgumentParser(description="Train DDPMNextTokenV1 model.")
     parser.add_argument('--train_size', type=int, default=16000, help='Number of training batches')
     parser.add_argument('--val_size', type=int, default=1600, help='Number of validation batches')
@@ -20,11 +16,13 @@ if __name__ == "__main__":
     train_size = args.train_size
     val_size = args.val_size
     batch_size = args.batch_size
-    # Initialize the DDPMNextTokenV1 pipeline
     
+    # Initialize the DDPMNextTokenV1 pipeline
     pipeline = DDPMNextTokenV1.DDPMNextTokenV1Pipeline()
+    # Configure the training parameters (TODO: make this configurable)
     pipeline.train_config.train_batch_size = batch_size
     pipeline.train_config.eval_batch_size = batch_size
+    # Get the dataloaders for training and validation
     train_dataloader = ModularCharatersDataLoader.get_modular_char_dataloader(dataset_name=dataset_name,
                                                                             split=train_split,
                                                                             image_size=pipeline.train_config.image_size,
@@ -35,12 +33,13 @@ if __name__ == "__main__":
                                                                             image_size=pipeline.train_config.image_size,
                                                                             batch_size=pipeline.train_config.eval_batch_size,
                                                                             shuffle=True)
+    # Start the training process
+    pipeline.train(train_dataloader=train_dataloader, 
+                   val_dataloader=val_dataloader, 
+                   train_size=train_size, 
+                   val_size=val_size)
 
-    pipeline.train(train_dataloader, val_dataloader, train_size = train_size, val_size = val_size)
 
-    # pipeline.load_config('/Data/quentin.leca/training_output/')
-    # for batch in val_dataloader:
-    #     images = pipeline(batch['input'], batch['prompt'])
-    #     print(images.shape)
-    #     # Image.fromarray(images)
-    #     break
+if __name__ == "__main__":
+    train_loop()
+  
