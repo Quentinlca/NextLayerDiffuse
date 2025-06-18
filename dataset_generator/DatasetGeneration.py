eb2d93a7a99ca2e90917eeaec30712d09511d15f
Quentinlca <quentin.leca@polytechnique.edu>
2025-06-18 16:19:25 +0200

    generated new dataset

--- dataset_generator/DatasetGeneration.py
+++ dataset_generator/DatasetGeneration.py
@@ -2,9 +2,15 @@ from src.DatasetGenerators import *
 
 if __name__ == '__main__':
     # Initialize the NextTokenGenerator with the desired parameters
-    generator = NextTokenGenerator('generated_datasets/modular_characters_v2',
-                                    output_image_size=128,
-                                    save_size=1000)
+    CharGenerator = CharacterGenerator(assets_dir='assets/kenney_new',
+                                   output_dir='generated_datasets/kenney_new')
     
-    generator.generate('characters.json',
+    CharGenerator.generate_small_dataset()
+    
+    TokenGenerator = NextTokenGenerator('generated_datasets/modular_characters_small',
+                                    output_image_size=128,
+                                    save_size=500)
+    
+    TokenGenerator.generate('generated_datasets/kenney_new/characters_sequences.json',
                        resume=False)
+    
