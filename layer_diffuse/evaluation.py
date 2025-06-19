af173b77c886a96fa505dbe6ecb90d31bad381f9
Quentin Leca <quentin.leca@polytechnique.edu>
2025-06-19 16:11:02 +0200

    Changed default parameter and parser

--- layer_diffuse/evaluation.py
+++ layer_diffuse/evaluation.py
@@ -1,19 +1,24 @@
 from models import DDPMNextTokenV1
 from models import DDPMNextTokenV2
-
 from data_loaders import ModularCharatersDataLoader
 import argparse
 
-def eval_loop(run_name:str, epoch:int):
-    split = f"train[0%:1%]"
+def eval_loop():
     parser = argparse.ArgumentParser(description="Train DDPMNextTokenV2 model.")
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training and validation')
     parser.add_argument('--dataset_name', type=str, default="QLeca/modular_characters_small", help='Dataset name to use for training')
+    parser.add_argument('--run_name', type=str, default='run_2025-06-19_14-12-46', help='Run name for loading the model')
+    parser.add_argument('--epoch', type=int, default=49, help='Epoch number to load the model from')
+    parser.add_argument('--num_inference_steps', type=int, default=1000, help='Number of inference steps for FID score calculation')
+    parser.add_argument('--split', type=str, default='train', help='Dataset split to use for evaluation')
     
     args = parser.parse_args()
-    # get the training and validation sizes from the command line arguments
     batch_size = args.batch_size
     dataset_name = args.dataset_name
+    run_name = args.run_name
+    epoch = args.epoch
+    num_inference_steps = args.num_inference_steps
+    split = args.split
     
     # Initialize the DDPMNextTokenV1 pipeline
     pipeline = DDPMNextTokenV2.DDPMNextTokenV2Pipeline()
@@ -30,7 +35,7 @@ def eval_loop(run_name:str, epoch:int):
     if pipeline.load_model_from_hub(run=run_name, epoch=epoch):
         print(f"Model loaded successfully from run {run_name} at epoch {epoch}.")
         FID_score = pipeline.get_FID_score(dataloader=dataloader,
-                               num_inference_steps=1000)
+                               num_inference_steps=num_inference_steps)
         if FID_score is not None:
             print(f"FID score for run {run_name} at epoch {epoch}: {FID_score}")
             stats = {'FID_score': {'run': run_name,
@@ -38,7 +43,7 @@ def eval_loop(run_name:str, epoch:int):
                                    'dataset_name': dataset_name,
                                    'split': split,
                                    'dataset_size': len(dataloader.dataset),
-                                   'num_inference_steps': 1000,
+                                   'num_inference_steps': num_inference_steps,
                                    'FID_score': FID_score}}
             if pipeline.save_stats(stats=stats, run=run_name, epoch=epoch):
                 print(f"Stats saved successfully for run {run_name} at epoch {epoch}.")
@@ -56,5 +61,5 @@ def eval_loop(run_name:str, epoch:int):
 
 
 if __name__ == "__main__":
-    eval_loop(run_name='run_2025-06-19_14-12-46', epoch=38)
+    eval_loop()
   
