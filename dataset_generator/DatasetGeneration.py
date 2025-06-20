5f92b6f07556cf548fe467365d92138a2698c9a0
Quentin Leca <quentin.leca@polytechnique.edu>
2025-06-20 15:26:56 +0200

    Changed generation file

--- dataset_generator/DatasetGeneration.py
+++ dataset_generator/DatasetGeneration.py
@@ -2,15 +2,11 @@ from src.DatasetGenerators import *
 
 if __name__ == '__main__':
     # Initialize the NextTokenGenerator with the desired parameters
-    CharGenerator = CharacterGenerator(assets_dir='assets/kenney_new',
-                                   output_dir='generated_datasets/kenney_new')
     
-    CharGenerator.generate_small_dataset()
-    
-    TokenGenerator = NextTokenGenerator('generated_datasets/modular_characters_small',
+    TokenGenerator = NextTokenGenerator('generated_datasets/modular_characters_hairs',
                                     output_image_size=128,
                                     save_size=500)
     
-    TokenGenerator.generate('generated_datasets/kenney_new/characters_sequences.json',
+    TokenGenerator.generate('characters.json',
                        resume=False)
     
