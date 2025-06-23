2ff6f7a8a72248b9b3cf23d61174d20d66cb1a6e
Quentinlca <quentin.leca@polytechnique.edu>
2025-06-23 15:31:13 +0200

    new training loop

--- layer_diffuse/training.py
+++ layer_diffuse/training.py
@@ -1,5 +1,7 @@
 from models import DDPMNextTokenV1
 from models import DDPMNextTokenV2
+from models import DDPMNextTokenV3
+
 
 from data_loaders import ModularCharatersDataLoader
 import argparse
@@ -7,11 +9,11 @@ import argparse
 def train_loop():
     val_split = f"train"
     train_split = f"train"
-    parser = argparse.ArgumentParser(description="Train DDPMNextTokenV2 model.")
+    parser = argparse.ArgumentParser(description="Train DDPMNextTokenV3 model.")
     parser.add_argument('--train_size', type=int, default=16000, help='Number of training batches')
     parser.add_argument('--val_size', type=int, default=1600, help='Number of validation batches')
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training and validation')
-    parser.add_argument('--dataset_name', type=str, default="QLeca/modular_characters_small", help='Dataset name to use for training')
+    parser.add_argument('--dataset_name', type=str, default="QLeca/modular_characters_hairs_RGB", help='Dataset name to use for training')
     parser.add_argument('--num_epochs', type=int, default=50, help='Training epochs')
     parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
     
@@ -25,7 +27,7 @@ def train_loop():
     lr = args.lr
     
     # Initialize the DDPMNextTokenV1 pipeline
-    pipeline = DDPMNextTokenV2.DDPMNextTokenV2Pipeline()
+    pipeline = DDPMNextTokenV3.DDPMNextTokenV3Pipeline()
     # Configure the training parameters (TODO: make this configurable)
     pipeline.train_config.train_batch_size = batch_size
     pipeline.train_config.eval_batch_size = batch_size
